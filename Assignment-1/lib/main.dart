3fda9b3d80bef16b61734e48114983fb76fe4fcd
Shaurya Choudhary <shaurya.src@gmail.com>
2022-01-10 14:54:30 +0530

    Fix continued input upon error

--- Assignment-1/lib/main.dart
+++ Assignment-1/lib/main.dart
@@ -10,52 +10,65 @@ void main() {
 
     double? itemPrice;
     int? itemQuantity;
+    String itemName;
     var itemType;
     bool errorFlag = false;
 
-    stdout.write("Enter the item name:\t");
-    String itemName = stdin.readLineSync().toString();
-    try {
-      if (itemName.isEmpty) {
-        throw Exception("Item name not found!");
+    do {
+      stdout.write("Enter the item name:\t");
+      itemName = stdin.readLineSync().toString();
+      try {
+        errorFlag = false;
+        if (itemName.isEmpty) {
+          throw Exception("Item name not found!");
+        }
+
+        if (Util.isNumeric(itemName)) {
+          throw Exception("Numeric value not allowed for item name!");
+        }
+      } catch (error) {
+        print("$error");
+        errorFlag = true;
       }
+    } while (errorFlag);
 
-      if (Util.isNumeric(itemName)) {
-        throw Exception("Numeric value not allowed for item name!");
+    do {
+      stdout.write("Enter the item price:\t");
+      try {
+        errorFlag = false;
+        itemPrice = double.parse(stdin.readLineSync().toString());
+        Util.isGreaterThanZero(itemPrice);
+      } catch (error) {
+        // if we provide other data type or a negative value
+        print("ItemPrice - $error");
+        errorFlag = true;
       }
-    } catch (error) {
-      print("$error");
-      errorFlag = true;
-    }
+    } while (errorFlag);
 
-    stdout.write("Enter the item price:\t");
-    try {
-      itemPrice = double.parse(stdin.readLineSync().toString());
-      Util.isGreaterThanZero(itemPrice);
-    } catch (error) {
-      // if we provide other data type or a negative value
-      print("ItemPrice - $error");
-      errorFlag = true;
-    }
+    do {
+      stdout.write("Enter the item quantity:\t");
+      try {
+        errorFlag = false;
+        itemQuantity = int.parse(stdin.readLineSync().toString());
+        Util.isGreaterThanZero(itemQuantity);
+      } catch (error) {
+        print("ItemQuantity - $error");
+        errorFlag = true;
+      }
+    } while (errorFlag);
 
-    stdout.write("Enter the item quantity:\t");
-    try {
-      itemQuantity = int.parse(stdin.readLineSync().toString());
-      Util.isGreaterThanZero(itemQuantity);
-    } catch (error) {
-      print("ItemQuantity - $error");
-      errorFlag = true;
-    }
-
-    stdout.write("Select the item type (1. raw | 2. manufactured | 3. imported):\t");
-    try {
-      int typeChoice = int.parse(stdin.readLineSync().toString());
-      itemType = ItemType.values[typeChoice - 1];
-    } catch (error) {
-      print("Error: Choose item type from {1, 2, or 3}");
-      print("$error");
-      errorFlag = true;
-    }
+    do {
+      stdout.write("Select the item type (1. raw | 2. manufactured | 3. imported):\t");
+      try {
+        errorFlag = false;
+        int typeChoice = int.parse(stdin.readLineSync().toString());
+        itemType = ItemType.values[typeChoice - 1];
+      } catch (error) {
+        print("Error: Choose item type from {1, 2, or 3}");
+        print("$error");
+        errorFlag = true;
+      }
+    } while (errorFlag);
 
     // ############################################
 
