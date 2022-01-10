aa61f25c84c3de73ceefeb59d0c6bad3e6ca98a5
Shaurya Choudhary <shaurya.src@gmail.com>
2022-01-10 17:02:14 +0530

    Add util function to check roll number

--- Assignment-2/lib/util.dart
+++ Assignment-2/lib/util.dart
@@ -1,3 +1,5 @@
+import 'user.dart';
+
 class Util {
   // Function to check if selected courses are valid
   static bool verifyCourses(List<String> courses) {
@@ -10,6 +12,14 @@ class Util {
       }
     } else {
       throw Exception("Please select atleast 4 courses.");
+    }
+    return true;
+  }
+
+  // Function to check if roll number already exists
+  static bool isRollNumberAlreadyPresent(int rollNumber) {
+    if (User.users.containsKey(rollNumber)) {
+      throw Exception("Roll number already present! Please try again.");
     }
     return true;
   }
