7223a78688c53fef6df855cf57798f44a2d1f3a0
Shaurya Choudhary <shaurya.src@gmail.com>
2022-01-11 10:30:41 +0530

    Add sorting methods

--- Assignment-2/lib/user.dart
+++ Assignment-2/lib/user.dart
@@ -9,12 +9,16 @@ class User {
   final List<String> courses;
 
   // Data structure to store students
-  static SplayTreeMap users = SplayTreeMap();
+  static HashSet registeredRollNumbers = new HashSet<int>();
+  static List<User> registeredUsers = [];
 
   // Constant constructur to initialize a User object
   // All the arguments are mandatory
   User(this.name, this.age, this.address, this.rollNumber, this.courses) {
-    users[rollNumber] = [name, age, address, courses];
+    registeredRollNumbers.add(this.rollNumber);
+    registeredUsers.add(this);
+    registeredUsers.sort((a, b) => a.rollNumber.compareTo(b.rollNumber));
+    registeredUsers.sort((a, b) => a.name.compareTo(b.name));
   }
 
   void showUserDetails() {
@@ -24,7 +28,15 @@ class User {
     print("$name \t $rollNumber \t\t $age \t $address \t ${courses.join(',')}");
   }
 
-  void showAllUsersInAscending() {}
+  void showAllUsersInAscending() {
+    for (User user in registeredUsers) {
+      print("${user.name} ${user.rollNumber}");
+    }
+  }
 
-  void showAllUsersInDescending() {}
+  void showAllUsersInDescending() {
+    for (int index = registeredUsers.length - 1; index >= 0; index--) {
+      print("${registeredUsers[index].name} ${registeredUsers[index].rollNumber}");
+    }
+  }
 }
