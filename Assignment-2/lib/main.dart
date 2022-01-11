762522ad23551c6225c87fca60841569c2310d9f
Shaurya Choudhary <shaurya.src@gmail.com>
2022-01-11 13:21:20 +0530

    Handled errors on age and roll input

--- Assignment-2/lib/main.dart
+++ Assignment-2/lib/main.dart
@@ -31,13 +31,15 @@ void addUser() {
     try {
       error = false;
       String ageString = stdin.readLineSync().toString();
+      // Check for string or null input
       if (int.tryParse(ageString) == null || ageString.isEmpty) {
         error = true;
         throw Exception("Invalid age. Please try again!");
       } else {
         age = int.parse(ageString);
-        if (Util.isGreaterThanZero(age) != true) {
+        if (!Util.isGreaterThanZero(age)) {
           error = true;
+          throw Exception("Age must be greater than zero.");
         }
       }
     } catch (exception) {
@@ -66,13 +68,22 @@ void addUser() {
     try {
       error = false;
       String rollNumberString = stdin.readLineSync().toString();
+      // Check for string or null input
       if (int.tryParse(rollNumberString) == null || rollNumberString.isEmpty) {
         error = true;
         throw Exception("Invalid Roll Number. Please try again!");
       } else {
         rollNumber = int.parse(rollNumberString);
+        if (Util.isGreaterThanZero(rollNumber)) {
+          if (Util.isRollNumberAlreadyPresent(rollNumber)) {
+            error = true;
+            throw Exception("Roll number already present! Please try again.");
+          }
+        } else {
+          error = true;
+          throw Exception("Roll number must be greater than zero.");
+        }
       }
-      Util.isRollNumberAlreadyPresent(rollNumber);
     } catch (exception) {
       print(exception);
     }
@@ -99,7 +110,9 @@ void saveUser() {
 }
 
 void main() {
-  print('''
+  bool noExit = true;
+  do {
+    print('''
 
 Please select an option:
 
@@ -111,46 +124,32 @@ Please select an option:
 
 ''');
 
-  stdout.write("Enter your choice:  ");
+    stdout.write("Enter your choice:  ");
 
-  try {
-    int choice = int.parse(stdin.readLineSync().toString());
+    try {
+      int choice = int.parse(stdin.readLineSync().toString());
 
-    switch (choice) {
-      case 1:
-        addUser();
-        break;
-      case 2:
-        break;
-      case 3:
-        break;
-      case 4:
-        break;
-      case 5:
-        break;
-      default:
-        throw Exception("Invalid choice! Select from - [1, 2, 3, 4, 5].");
+      switch (choice) {
+        case 1:
+          addUser();
+          break;
+        case 2:
+          break;
+        case 3:
+          break;
+        case 4:
+          break;
+        case 5:
+          noExit = false;
+          break;
+        default:
+          throw Exception("Invalid choice! Select from - [1, 2, 3, 4, 5].");
+      }
+    } catch (error) {
+      print("$error");
     }
-  } catch (error) {
-    print("$error");
-  }
+  } while (noExit);
 
-  // var test = User("Amit", 12, "Chennai", 17, ["A", "B", "E", "F"]);
-  // var test1 = User("Rahul", 26, "Mumbai", 19, ["B", "E", "F", "D", "C"]);
-  // var test2 = User("Max", 19, "Delhi", 12, ["E", "F", "A", "B"]);
-  // var test3 = User("Zyan", 19, "Delhi", 12, ["E", "F", "A", "B"]);
-  // var test4 = User("Aam", 19, "Delhi", 17, ["E", "F", "A", "B"]);
-  // var test5 = User("Aam", 19, "Delhi", 13, ["E", "F", "A", "B"]);
-
-  // try {
-  //   Util.verifyCourses(test.courses);
-  //   test.showUserDetails();
-  // } catch (error) {
-  //   print(error);
-  // }
-
-  // print(User.users);
-  // print(User.users[1].name);
   for (User user in User.registeredUsers) {
     user.showUserDetails();
   }
