e5e52050ed263125509f6a8571de5c69461bffcc
Shaurya Choudhary <shaurya.src@gmail.com>
2022-01-13 12:44:04 +0530

    Define graph class

--- Assignment-3/lib/graph.dart
+++ Assignment-3/lib/graph.dart
@@ -1,3 +1,22 @@
 import 'dart:collection';
 
-class Graph {}
+class Node {
+  final String id;
+  final String label;
+  final Map<String, dynamic>? additionalInfo;
+
+  Node(this.id, this.label, [this.additionalInfo]);
+}
+
+class Graph {
+  final Node root = Node('0', 'root');
+  Map<Node, List<Node>> graph = {};
+
+  void addRoot(Node newNode) {
+    if (graph.isEmpty) {
+      this.graph[root] = [newNode];
+    } else {
+      this.graph[root]!.add(newNode);
+    }
+  }
+}
