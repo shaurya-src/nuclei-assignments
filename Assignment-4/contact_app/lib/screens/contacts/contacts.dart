f76dbab8f8ecedb99bfbc8966455f714949fecbb
Shaurya Choudhary <shaurya.src@gmail.com>
2022-01-28 14:45:30 +0530

    Add logic to get contacts

--- Assignment-4/contact_app/lib/screens/contacts/contacts.dart
+++ Assignment-4/contact_app/lib/screens/contacts/contacts.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:contacts_service/contacts_service.dart';
 import 'package:permission_handler/permission_handler.dart';
+import './contact_item.dart';
 
 class Contacts extends StatefulWidget {
   const Contacts({Key? key}) : super(key: key);
@@ -10,12 +11,43 @@ class Contacts extends StatefulWidget {
 }
 
 class _ContactsState extends State<Contacts> {
+  List<Contact> contactList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    getPermissions();
+  }
+
+  getPermissions() async {
+    if (await Permission.contacts.request().isGranted) {
+      getAllContacts();
+    }
+  }
+
+  getAllContacts() async {
+    List<Contact> _contacts = (await ContactsService.getContacts()).toList();
+    setState(() {
+      contactList = _contacts;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Contacts'),
         centerTitle: true,
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(5),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            ...contactList.map((contact) => ContactItem(contact)),
+          ],
+        ),
       ),
     );
   }
