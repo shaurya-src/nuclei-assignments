f93edf0aeb799013765a12825b8deb96cb29f9f5
Shaurya Choudhary <shaurya.src@gmail.com>
2022-01-31 14:15:10 +0530

    Structure contact details page

--- Assignment-4/contact_app/lib/screens/contact_detail/contact_detail.dart
+++ Assignment-4/contact_app/lib/screens/contact_detail/contact_detail.dart
@@ -1,5 +1,6 @@
 import 'package:contacts_service/contacts_service.dart';
 import 'package:flutter/material.dart';
+import '../contacts/contact_item.dart';
 
 class ContactDetail extends StatelessWidget {
   final Contact contactItem;
@@ -7,18 +8,82 @@ class ContactDetail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    String name = contactItem.displayName!;
+    String phone = contactItem.phones!.first.value!;
+    String email = contactItem.emails!.first.value!;
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Contact Details'),
+        title: const Text(' '),
+        backgroundColor: Colors.cyan[700],
         centerTitle: true,
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: const [
-          Text('First Contact'),
+        children: [
+          imageBanner(contactItem),
+          infoBanner('Name', name, Icons.people_alt_rounded),
+          infoBanner('Phone', phone, Icons.phone_android_rounded),
+          infoBanner('E-Mail', email, Icons.email_rounded),
         ],
       ),
+    );
+  }
+
+  Widget infoBanner(String title, String info, IconData icon) {
+    return Container(
+      margin: const EdgeInsets.all(20),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Row(
+            children: [
+              Icon(icon),
+              const SizedBox(
+                width: 10,
+              ),
+              Text(
+                title,
+                style: _titleStyle(),
+              ),
+            ],
+          ),
+          Text(
+            info,
+            style: _infoStyle(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget imageBanner(Contact contactInfo) {
+    return Container(
+      constraints: const BoxConstraints.expand(
+        height: 300,
+      ),
+      decoration: const BoxDecoration(
+        color: Colors.grey,
+      ),
+      child: ContactItem.getAvatar(contactInfo),
+    );
+  }
+
+  TextStyle _infoStyle() {
+    return const TextStyle(
+      fontFamily: 'Raleway',
+      fontSize: 16,
+      fontWeight: FontWeight.w300,
+    );
+  }
+
+  TextStyle _titleStyle() {
+    return const TextStyle(
+      fontFamily: 'Raleway',
+      fontSize: 22,
+      fontWeight: FontWeight.w600,
     );
   }
 }
