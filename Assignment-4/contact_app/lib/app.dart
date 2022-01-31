6028ed5cb2346509aca4f0fe06d585e0f277f405
Shaurya Choudhary <shaurya.src@gmail.com>
2022-01-31 13:15:58 +0530

    Add routing

--- Assignment-4/contact_app/lib/app.dart
+++ Assignment-4/contact_app/lib/app.dart
@@ -1,21 +1,41 @@
 import 'package:flutter/material.dart';
 import 'screens/contacts/contacts.dart';
+import 'screens/contact_detail/contact_detail.dart';
+
+const contactsRoute = '/';
+const contactDetailRoute = '/contact_detail';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Contact App',
+      onGenerateRoute: _routes(),
       theme: _theme(),
-      home: const Contacts(),
     );
+  }
+
+  RouteFactory _routes() {
+    return (settings) {
+      final arguments = settings.arguments as Map<String, dynamic>?;
+      Widget screen;
+
+      switch (settings.name) {
+        case contactsRoute:
+          screen = const Contacts();
+          break;
+        case contactDetailRoute:
+          screen = ContactDetail(arguments!['contact']);
+          break;
+        default:
+          return null;
+      }
+      return MaterialPageRoute(builder: (BuildContext context) => screen);
+    };
   }
 
   ThemeData _theme() {
     return ThemeData(
-      appBarTheme: const AppBarTheme(
-        backgroundColor: Colors.blue,
-      ),
+      appBarTheme: const AppBarTheme(),
     );
   }
 }
