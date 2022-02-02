8a4c7c6c38dd4ca93715b7cad390bc5d283d4888
Shaurya Choudhary <shaurya.src@gmail.com>
2022-02-02 20:42:41 +0530

    Add changeNotifier view model

--- Assignment-4/contact_app/lib/screens/contacts/view_model/contacts_provider.dart
+++ Assignment-4/contact_app/lib/screens/contacts/view_model/contacts_provider.dart
@@ -4,21 +4,33 @@ import 'package:permission_handler/permission_handler.dart';
 
 class ContactsProvider extends ChangeNotifier {
   List<Contact> _contactList = [];
+  bool _isContactLoaded = false;
 
   // Initial method to get permission
   void getPermission() async {
     if (await Permission.contacts.request().isGranted) {
-      _getAllContacts();
+      getAllContacts();
+      notifyListeners();
     }
   }
 
   // Method to get list of all contacts
-  void _getAllContacts() async {
+  void getAllContacts() async {
     List<Contact> _contacts = (await ContactsService.getContacts()).toList();
     _contactList = _contacts;
+    _isContactLoaded = true;
     notifyListeners();
   }
 
-  // Getter for contact list
+  // Search Contacts
+  void searchContacts() async {
+    String _query = "";
+    List<Contact> _searchedContacts = await ContactsService.getContacts(query: _query);
+    _contactList = _searchedContacts;
+    notifyListeners();
+  }
+
+  // Getter for contactList and status
   List<Contact> get contactList => _contactList;
+  bool get isContactLoaded => _isContactLoaded;
 }
