16650eefa4f0857f8907636acbbaaf52b2347be4
Shaurya Choudhary <shaurya.src@gmail.com>
2022-02-03 12:25:47 +0530

    Add delete confirm dialog box

--- Assignment-4/contact_app/lib/screens/contacts/widget/delete_button.dart
+++ Assignment-4/contact_app/lib/screens/contacts/widget/delete_button.dart
@@ -9,7 +9,39 @@ class DeleteButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return IconButton(
       icon: const Icon(Icons.delete_forever_rounded),
-      onPressed: () => print("foobar"),
+      onPressed: () => _showDeleteDialog(context),
+    );
+  }
+
+  Future<void> _showDeleteDialog(BuildContext context) {
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("Cofirm Contact Deletion"),
+          content: Text(
+            "Do you really want to delete this contact: ${contactInfo.displayName}? ",
+          ),
+          actions: [
+            _getConfirmButton(context),
+            _getCancelButton(context),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget _getConfirmButton(BuildContext context) {
+    return TextButton(
+      child: const Text("Confirm"),
+      onPressed: () => Navigator.of(context).pop(),
+    );
+  }
+
+  Widget _getCancelButton(BuildContext context) {
+    return TextButton(
+      child: const Text("Cancel"),
+      onPressed: () => Navigator.of(context).pop(),
     );
   }
 }
