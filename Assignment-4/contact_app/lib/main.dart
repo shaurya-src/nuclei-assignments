4eb9a30cf4744703e0415ec06941d62a27e5cfbd
Shaurya Choudhary <shaurya.src@gmail.com>
2022-02-04 11:34:01 +0530

    Register hive Adapter

--- Assignment-4/contact_app/lib/main.dart
+++ Assignment-4/contact_app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:contact_app/screens/contacts/model/user_contact.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
@@ -6,6 +7,8 @@ import './app.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
+
+  Hive.registerAdapter(UserContactAdapter());
 
   runApp(const MyApp());
 }
