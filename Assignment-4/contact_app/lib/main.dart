dc8ef56766f6491b4d2e6ecd60d068a9a9a23fe5
Shaurya Choudhary <shaurya.src@gmail.com>
2022-02-04 11:03:01 +0530

    Fix hive initialization

--- Assignment-4/contact_app/lib/main.dart
+++ Assignment-4/contact_app/lib/main.dart
@@ -1,14 +1,11 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:hive/hive.dart';
-import 'package:path_provider/path_provider.dart' as path_provider;
+import 'package:hive_flutter/hive_flutter.dart';
 
 import './app.dart';
 
 Future<void> main() async {
-  Directory directory = await path_provider.getApplicationDocumentsDirectory();
-  Hive.init(directory.path);
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
 
   runApp(const MyApp());
 }
