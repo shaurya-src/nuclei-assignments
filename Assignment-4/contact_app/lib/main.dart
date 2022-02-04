fa14cb0ba68d86b630c75d41a2a9da9edffe7999
Shaurya Choudhary <shaurya.src@gmail.com>
2022-02-04 10:53:09 +0530

    Initialize hive for app

--- Assignment-4/contact_app/lib/main.dart
+++ Assignment-4/contact_app/lib/main.dart
@@ -1,4 +1,14 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
+
 import './app.dart';
 
-void main() => runApp(const MyApp());
+Future<void> main() async {
+  Directory directory = await path_provider.getApplicationDocumentsDirectory();
+  Hive.init(directory.path);
+
+  runApp(const MyApp());
+}
